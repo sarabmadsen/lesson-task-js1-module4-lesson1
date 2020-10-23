242489952a36faf214f91f6826b1bb75136d1f63
Connor O'Brien <connor@hifranc.com>
2020-10-23 11:39:21 +0200

    Add answer

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript 1 - Module 4
 
-## Lesson Task 1 Question
+## Lesson Task 1 Answer
 
 Make a call to the following endpoint:
 
@@ -10,4 +10,4 @@ https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/nbateams
 
 Display the team name and city of the first 15 results, but exclude any team whose name begins with `c`.
 
-(15 teams should always be displayed, but none of their names should start with `c`).
+There will be a maximum of 15 results displayed if no teams' names begin with "C", and less than 15 displayed if there are teams whose names begin with "C".
---
+++ js/script.js
@@ -0,0 +1,56 @@
+const url = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/nbateams";
+
+const resultsContainer = document.querySelector(".results");
+
+// we need the code in an async function as we are using the await keyword
+async function fetchTeams() {
+    try {
+        const response = await fetch(url);
+
+        const json = await response.json();
+
+        // always log and inspect the data you get from an API call to see what properties it has
+        console.log(json);
+
+        const teams = json.data;
+
+        resultsContainer.innerHTML = "";
+
+        for (let i = 0; i < teams.length; i++) {
+            // we only want to display a maximum of 15 teams
+            // there will be less than 15 if any team names begin with "C"
+            // use break to leave the loop
+            if (i === 15) {
+                break;
+            }
+
+            const teamName = teams[i].full_name;
+            const city = teams[i].city;
+
+            // startsWith documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+
+            // we are checking for small "c" and big "C"
+            if (teamName.startsWith("c") || teamName.startsWith("C")) {
+                // i = i - 1;
+                continue;
+            }
+
+            // instead of checking for both small "c" and big "C" we can make the teamName lowercase and just check for "c"
+            // if (teamName.toLowerCase().startsWith("c")) {
+            //     continue;
+            // }
+
+            resultsContainer.innerHTML += `<div class="card">
+                                            <h4>${teamName}</h4>
+                                            <p>${city}</p>
+                                        </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+        resultsContainer.innerHTML = message("error", error);
+    }
+}
+
+// call the function
+// without this nothing will happen
+fetchTeams();
